123f69b67e2dae4b8c91c0107e9ff1f71fb461f7
dhunganabibek <Dhunganabibek2016@gmail.com>
2024-07-12 21:42:59 -0500

    using foreach loop in integer array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 Console.WriteLine("Hello, World!");
+
+int[] nums = {1,2,3,4,5,6,7,8,9};
+
+foreach(int n in nums){
+    System.Console.WriteLine(n);
+}
