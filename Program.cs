5ca796d2d6840e9455d971317de8b1001ee0c6cb
dhunganabibek <Dhunganabibek2016@gmail.com>
2024-07-12 21:45:48 -0500

    changing loop syntax to linq syntax

--- Program.cs
+++ Program.cs
@@ -3,6 +3,18 @@ Console.WriteLine("Hello, World!");
 
 int[] nums = {1,2,3,4,5,6,7,8,9};
 
+
+// using forreach loop
 foreach(int n in nums){
-    System.Console.WriteLine(n);
+    if(n % 2 == 0)
+        Console.WriteLine(n);
+}
+
+// using linq query
+var query = from n in nums
+            where n % 2 == 0
+            select n;
+
+foreach(int n in query){
+    Console.WriteLine(n);
 }
