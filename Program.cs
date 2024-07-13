6a0bd430c49ad8cc1ad7a39b209d4666d72d03a3
dhunganabibek <Dhunganabibek2016@gmail.com>
2024-07-12 21:59:28 -0500

    adding order by in linq queries

--- Program.cs
+++ Program.cs
@@ -5,16 +5,18 @@ int[] nums = {1,2,3,4,5,6,7,8,9};
 
 
 // using forreach loop
-foreach(int n in nums){
-    if(n % 2 == 0)
-        Console.WriteLine(n);
-}
+// foreach(int n in nums){
+//     if(n % 2 == 0)
+//         Console.WriteLine(n);
+// }
 
 // using linq query
 var query = from n in nums
             where n % 2 == 0
+            orderby n descending
             select n;
 
+// query is executed here
 foreach(int n in query){
     Console.WriteLine(n);
 }
